0bd82de2f07a7c1b128c3e55faf424d0c330c927
Owen Wright <owenwright@seattleacademy.org>
2024-01-25 10:32:03 -0800

    boxes and math

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const numberBox = document.getElementById('guess');
 const feedbackBox = document.getElementById('feedback');
 
+document.getElementById('guess').style.fontSize = "x-large";
+
 const newNumber = Math.floor(Math.random()*100);
 
 numberBox.addEventListener('change', checkNumber);
@@ -18,3 +20,28 @@ function checkNumber() {
     }
 }
 
+const calcBox = document.getElementById('calc');
+const functionBox = document.getElementById('function');
+const outputBox = document.getElementById('output')
+
+document.getElementById('calc').style.fontSize = "x-large";
+document.getElementById('output').style.fontSize = "x-large";
+
+functionBox.addEventListener('change', function1)
+calcBox.addEventListener('change',function1)
+
+function function1() {
+    const number = calcBox.value;
+    if( functionBox.value == "cube"){
+        outputBox.innerText = number**3
+    }
+    else if ( functionBox.value == "square"){
+        outputBox.innerText = number**2
+    }
+    else if ( functionBox.value == "2x+7"){
+        outputBox.innerText = 2*number+7
+    }
+    else if ( functionBox.value == "double"){
+        outputBox.innerText = number*2
+    }
+}
